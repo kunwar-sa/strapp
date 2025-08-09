ab533ca6ee97731d7cf854517b2933b3fab96abe
Kunwar Prakhar Singh <singhkunwarprakhar@gmail.com>
2025-08-09 22:21:37 +0530

    initial commit

---
+++ src/main/java/com/prototype/strapp/StrappApplication.java
@@ -0,0 +1,13 @@
+package com.prototype.strapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prototype/strapp/StrappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prototype.strapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
